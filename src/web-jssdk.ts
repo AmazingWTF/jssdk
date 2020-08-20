8ea197d6ded40df23150e20d84232b0bb037bbb3
wenjie.geng <gwjlib@163.com>
2020-08-20 18:39:17 +0800

    引入core-js兼容新语法

--- src/web-jssdk.ts
+++ src/web-jssdk.ts
@@ -1,3 +1,4 @@
+import './high-level-syntax-shim'
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
@@ -5,5 +6,12 @@ export default class DummyClass {
   readonly name: string
   constructor (name: string) {
     this.name = name
+  }
+
+  getPromise () {
+    return new Promise((resolve, reject) => {
+      const arr = [1, 3, 5]
+      resolve(arr.find(i => i > 3))
+    })
   }
 }
