4affd666bdd85c406d70850951b47e9458531aba
wenjie.geng <gwjlib@163.com>
2020-08-24 18:53:32 +0800

    用core-js兼容ES6/7语法

--- src/web-jssdk.ts
+++ src/web-jssdk.ts
@@ -1,7 +1,6 @@
 import './high-level-syntax-shim'
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
+import "core-js/fn/array.find"
 export default class DummyClass {
   readonly name: string
   constructor (name: string) {
